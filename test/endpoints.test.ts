eec0b5366589169bb0d43f441683e4622c8dd571
rxrosli <raymark.roslin@gmail.com>
2022-07-05 07:36:55 +0800

    test: create endpoint unit test description

---
+++ test/endpoints.test.ts
@@ -0,0 +1,30 @@
+describe('Given a bowling game service', () => {
+	it('Should allow to create a game.', () => {
+		expect(false).toBe(true);
+	});
+	it('Should allow to delete a game', () => {
+		expect(false).toBe(true);
+	});
+	it('Should allow to read a game', () => {
+		expect(false).toBe(true);
+	});
+	it('Should allow to update a game', () => {
+		expect(false).toBe(true);
+	});
+
+	describe('When a bowling game exists in service', () => {
+		it('Should allow to create a frame score', () => {
+			expect(false).toBe(true);
+		});
+		it('Should allow to delete a frame score', () => {
+			// What is the proper behavior when a frame is deleted?
+			expect(false).toBe(true);
+		});
+		it('Should allow to read a frame score', () => {
+			expect(false).toBe(true);
+		});
+		it('Should allow to update a frame score', () => {
+			expect(false).toBe(true);
+		});
+	});
+});
