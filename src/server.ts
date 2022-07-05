3a5417dff0623a0609e6b3df79459dce18ddec37
rxrosli <raymark.roslin@gmail.com>
2022-07-05 17:02:43 +0800

    refactor: separated express app so it can be testable

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import endpoints from './endpoints';
 
+export const app = express();
+const PORT = process.env.PORT || '3000';
+
+const corsOptions = {
+	origin: '*',
+	methods: 'GET,HEAD,PUT,POST,DELETE',
+	preflightContinue: false,
+	optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api', endpoints);
+
 async function main() {
-	const app = express();
-	const PORT = process.env.PORT || '3000';
-
-	const corsOptions = {
-		origin: '*',
-		methods: 'GET,HEAD,PUT,POST,DELETE',
-		preflightContinue: false,
-		optionsSuccessStatus: 204
-	};
-
-	app.use(cors(corsOptions));
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: false }));
-	app.use('/api', endpoints);
-
 	app.listen(PORT, () => {
 		console.info(`Server ready at port ${PORT}`);
 	});
